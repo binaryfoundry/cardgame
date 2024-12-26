6a64228cc26fdb39b2e16ab99bfe827f9ca29d6f
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2024-12-26 10:51:29 +0000

    Update Comments on TestGame from Spec

--- tests/GameTest.cpp
+++ tests/GameTest.cpp
@@ -3,6 +3,34 @@
 #include <iostream>
 #include <cassert>
 
+/*
+ * From the requirements:
+ * - We start with the deck of 52 cards, each uniquely numbered from 1 to 52
+ * - The deck is shuffled
+ * - We deal out those cards between the 2 players.Each player gets half the deck.
+ * - On each turn of the game, both players turn over their top most card and they compare the value of those cards.
+ * - The player with the higher valued card "wins" the round and gets a point.The two cards being compared are discarded.
+ * - Rounds are played until all the cards are discarded.
+ *
+ * Therefore using the default constructor to shuffle the deck there can not be draws in a round.
+*/
+
+void TestGame()
+{
+    for (uint32_t i = 0; i < 20; i++)
+    {
+        Game game;
+        const Player winner = game.PlayGame([&](const size_t played_round, const int32_t player_1_card, const int32_t player_2_card, const Player winner)
+        {
+            assert(winner != Player::None);
+        });
+
+        assert(game.GetPlayer1Score() + game.GetPlayer2Score() == 26);
+    }
+
+    std::cout << "TestGame passed." << std::endl;
+}
+
 void TestFullDeckDraw()
 {
     Game game({
@@ -70,6 +98,7 @@ void TestUnusualValues()
 }
 
 int main(int argc, char* argv[]) {
+    TestGame();
     TestFullDeckDraw();
     TestFullDeckPlayer1Wins();
     TestFullDeckPlayer2Wins();
