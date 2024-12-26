7c96ce650d30a7983e41e6ff1294f98672449203
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2024-12-26 10:12:47 +0000

    Add Tests for Either Player Winning All Rounds

--- tests/GameTest.cpp
+++ tests/GameTest.cpp
@@ -18,7 +18,39 @@ void TestFullDeckDraw()
     std::cout << "TestFullDeckDraw passed." << std::endl;
 }
 
+void TestFullDeckPlayer1Wins()
+{
+    Game game({
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    });
+    const Player winner = game.PlayGame();
+
+    assert(winner == Player::Player1);
+    assert(game.GetPlayer1Score() == 26);
+    assert(game.GetPlayer2Score() == 0);
+
+    std::cout << "TestFullDeckPlayer1Wins passed." << std::endl;
+}
+
+void TestFullDeckPlayer2Wins()
+{
+    Game game({
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    });
+    const Player winner = game.PlayGame();
+
+    assert(winner == Player::Player2);
+    assert(game.GetPlayer1Score() == 0);
+    assert(game.GetPlayer2Score() == 26);
+
+    std::cout << "TestFullDeckPlayer2Wins passed." << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     TestFullDeckDraw();
+    TestFullDeckPlayer1Wins();
+    TestFullDeckPlayer2Wins();
     return 0;
 }
