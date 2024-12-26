b8ff683740c4224fc6a2721021c0987774589950
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2024-12-26 10:32:54 +0000

    Add Test for Unusual Values

--- tests/GameTest.cpp
+++ tests/GameTest.cpp
@@ -48,9 +48,31 @@ void TestFullDeckPlayer2Wins()
     std::cout << "TestFullDeckPlayer2Wins passed." << std::endl;
 }
 
+constexpr uint32_t uint32_t_max_value = std::numeric_limits<uint32_t>::max();
+constexpr uint32_t uint32_t_min_value = std::numeric_limits<uint32_t>::min();
+constexpr uint32_t uint32_t_infinity = std::numeric_limits<uint32_t>::infinity();
+constexpr uint32_t uint32_t_nan = std::numeric_limits<uint32_t>::quiet_NaN();
+
+// This isn't a problem for uint32_t, but it's a good habit to have incase the type ever changes.
+void TestUnusualValues()
+{
+    Game game({
+        uint32_t_max_value, uint32_t_min_value, uint32_t_nan, 1, uint32_t_infinity, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        uint32_t_min_value, uint32_t_max_value, 1, uint32_t_nan, 0, uint32_t_infinity, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    });
+    const Player winner = game.PlayGame();
+
+    assert(winner == Player::None);
+    assert(game.GetPlayer1Score() == 25);
+    assert(game.GetPlayer2Score() == 25);
+
+    std::cout << "TestUnusualValues passed." << std::endl;
+}
+
 int main(int argc, char* argv[]) {
     TestFullDeckDraw();
     TestFullDeckPlayer1Wins();
     TestFullDeckPlayer2Wins();
+    TestUnusualValues();
     return 0;
 }
